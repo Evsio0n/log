341ca719db1b7762af13cdeb749a4fea002b1ac2
evsio0n <bbq2001820@gmail.com>
2021-08-02 16:42:43 +0800

    Add options to decide whether show log date.

--- log.go
+++ log.go
@@ -10,9 +10,14 @@ import (
 )
 
 var isDEBUG = true
+var isShowDate = false
 
 func SetDebug(debug bool) {
 	isDEBUG = debug
+}
+
+func IsShowDate(ShowDate bool) {
+	isShowDate = ShowDate
 }
 func Info(arg ...interface{}) {
 	if isDEBUG {
@@ -45,8 +50,12 @@ func Fatal(arg ...interface{}) {
 
 func buildString(level string, args []interface{}) string {
 	var tag []interface{}
-
-	tag = append(tag, time.Now().Format("2006-01-02 15:04:05.000000"), level, getPosition(), " -> ")
+	switch isShowDate {
+	case true:
+		tag = append(tag, time.Now().Format("2006-01-02 15:04:05.000000"), level, getPosition(), " -> ")
+	case false:
+		tag = append(tag, level, getPosition(), " -> ")
+	}
 	s := fmt.Sprint(tag...) + fmt.Sprint(args...)
 	return s
 }
