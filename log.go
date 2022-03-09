856743806fca8f97c0370a9be37d8d892d771053
evsio0n <bbq2001820@gmail.com>
2022-03-09 16:34:50 +0800

    1.Add Panic() function if wants to panic here with error. 2.Add syslog if user wants to log to syslog instead of std.

--- log.go
+++ log.go
@@ -2,15 +2,30 @@ package log
 
 import (
 	"fmt"
-	"log"
+	"log/syslog"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
 )
 
-var isDEBUG = true
+var isDEBUG = false
 var isShowDate = false
+var isSyslog = false
+var syslogTag = "syslog"
+var dial *syslog.Writer
+var err error
+var isShowLogCatagory = false
+
+func SetSyslog(debug bool, tagName string) error {
+	isSyslog = debug
+	syslogTag = tagName
+	dial, err = syslog.Dial("", "", syslog.LOG_DAEMON, syslogTag)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func SetDebug(debug bool) {
 	isDEBUG = debug
@@ -19,33 +34,101 @@ func SetDebug(debug bool) {
 func IsShowDate(ShowDate bool) {
 	isShowDate = ShowDate
 }
+
+func IsShowLogCatagory(ShowLogCatagory bool) {
+	isShowLogCatagory = ShowLogCatagory
+}
+
 func Info(arg ...interface{}) {
-	if isDEBUG {
-		s := buildString(" [INFO]  ", arg)
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [INFO]  ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	if isSyslog {
+		dial.Info(s)
+	} else {
 		fmt.Printf("%c[1;32m%s%c[0m\n", 0x1B, s, 0x1B)
 	}
+
 }
 
 func Debug(arg ...interface{}) {
 	if isDEBUG {
-		s := buildString(" [DEBUG] ", arg)
-		fmt.Printf("%c[1;34m%s%c[0m\n", 0x1B, s, 0x1B)
+		var s string
+		if isShowLogCatagory {
+			s = buildString(" [DEBUG] ", arg)
+		} else {
+			s = buildString("", arg)
+		}
+		if isSyslog {
+			dial.Debug(s)
+		} else {
+			fmt.Printf("%c[1;36m%s%c[0m\n", 0x1B, s, 0x1B)
+		}
 	}
 }
 
 func Warn(arg ...interface{}) {
-	s := buildString(" [WARN]  ", arg)
-	fmt.Printf("%c[1;33m%s%c[0m\n", 0x1B, s, 0x1B)
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [WARN]  ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	if isSyslog {
+		dial.Warning(s)
+	} else {
+		fmt.Printf("%c[1;33m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
 }
 
 func Error(arg ...interface{}) {
-	s := buildString(" [ERROR] ", arg)
-	fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [ERROR] ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	if isSyslog {
+		dial.Err(s)
+	} else {
+		fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
 }
 
 func Fatal(arg ...interface{}) {
-	s := buildString(" [FATAL] ", arg)
-	log.Fatalf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [FATAL] ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	if isSyslog {
+		dial.Crit(s)
+	} else {
+		fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
+}
+
+func Panic(arg ...interface{}) {
+	var s string
+
+	switch isShowLogCatagory {
+	case true:
+		s = buildString(" [PANIC] ", arg)
+	default:
+		s = buildString("", arg)
+	}
+
+	switch isSyslog {
+	case true:
+		dial.Crit(s)
+	default:
+		fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
+	panic(s)
 }
 
 func buildString(level string, args []interface{}) string {
