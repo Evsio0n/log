33602e5c7ec7c2093205444f624441f5daff7152
evsio0n <bbq2001820@gmail.com>
2023-03-02 01:49:29 +0800

    Update since windows no support with syslog

--- log_windows.go
+++ log_windows.go
@@ -4,3 +4,85 @@
 package log
 
 var isSyslog = false
+
+func SetSyslog(debug bool, tagName string) error {
+	isSyslog = debug
+	syslogTag = tagName
+	dial, err = syslog.Dial("", "", syslog.LOG_DAEMON, syslogTag)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Info(arg ...interface{}) {
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [INFO]  ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	fmt.Printf("%c[1;32m%s%c[0m\n", 0x1B, s, 0x1B)
+
+}
+
+func Debug(arg ...interface{}) {
+	if isDEBUG {
+		var s string
+		if isShowLogCatagory {
+			s = buildString(" [DEBUG] ", arg)
+		} else {
+			s = buildString("", arg)
+		}
+		fmt.Printf("%c[1;36m%s%c[0m\n", 0x1B, s, 0x1B)
+
+	}
+}
+
+func Warn(arg ...interface{}) {
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [WARN]  ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	fmt.Printf("%c[1;33m%s%c[0m\n", 0x1B, s, 0x1B)
+
+}
+
+func Error(arg ...interface{}) {
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [ERROR] ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+
+}
+
+func Fatal(arg ...interface{}) {
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [FATAL] ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+
+}
+
+func Panic(arg ...interface{}) {
+	var s string
+
+	switch isShowLogCatagory {
+	case true:
+		s = buildString(" [PANIC] ", arg)
+	default:
+		s = buildString("", arg)
+	}
+
+	fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+
+	panic(s)
+}
--- syslog.go
+++ syslog.go
@@ -3,7 +3,11 @@
 
 package log
 
-import "log/syslog"
+import (
+	"fmt"
+	"log/syslog"
+	_ "time"
+)
 
 var isSyslog = false
 var syslogTag = "syslog"
@@ -17,4 +21,96 @@ func SetSyslog(debug bool, tagName string) error {
 		return err
 	}
 	return nil
+}
+
+func Info(arg ...interface{}) {
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [INFO]  ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	if isSyslog {
+		dial.Info(s)
+	} else {
+		fmt.Printf("%c[1;32m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
+
+}
+
+func Debug(arg ...interface{}) {
+	if isDEBUG {
+		var s string
+		if isShowLogCatagory {
+			s = buildString(" [DEBUG] ", arg)
+		} else {
+			s = buildString("", arg)
+		}
+		if isSyslog {
+			dial.Debug(s)
+		} else {
+			fmt.Printf("%c[1;36m%s%c[0m\n", 0x1B, s, 0x1B)
+		}
+	}
+}
+
+func Warn(arg ...interface{}) {
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [WARN]  ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	if isSyslog {
+		dial.Warning(s)
+	} else {
+		fmt.Printf("%c[1;33m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
+}
+
+func Error(arg ...interface{}) {
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [ERROR] ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	if isSyslog {
+		dial.Err(s)
+	} else {
+		fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
+}
+
+func Fatal(arg ...interface{}) {
+	var s string
+	if isShowLogCatagory {
+		s = buildString(" [FATAL] ", arg)
+	} else {
+		s = buildString("", arg)
+	}
+	if isSyslog {
+		dial.Crit(s)
+	} else {
+		fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
+}
+
+func Panic(arg ...interface{}) {
+	var s string
+
+	switch isShowLogCatagory {
+	case true:
+		s = buildString(" [PANIC] ", arg)
+	default:
+		s = buildString("", arg)
+	}
+
+	switch isSyslog {
+	case true:
+		dial.Crit(s)
+	default:
+		fmt.Printf("%c[1;31m%s%c[0m\n", 0x1B, s, 0x1B)
+	}
+	panic(s)
 }
